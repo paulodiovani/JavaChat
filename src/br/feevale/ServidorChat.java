199991fd3044f03eb3e62000640ee33362f5e7ed
Paulo Diovani Gonçalves <paulo@diovani.com>
2014-06-27 22:57:45 -0300

    Removidos fontes e recursos em desuso;

--- src/br/feevale/ServidorChat.java
+++ src/br/feevale/ServidorChat.java
@@ -12,82 +12,82 @@ import java.net.Socket;
  */
 public class ServidorChat extends Thread {
 
-	/**
-	 * Instancia singleton da classe
-	 */
-	private static ServidorChat instance;
+    /**
+     * Instancia singleton da classe
+     */
+    private static ServidorChat instance;
 
-	private Integer porta;
-	
-	private String nomeServidor;
+    private Integer porta;
+    
+    private String nomeServidor;
 
-	private Socket cliente;
+    private Socket cliente;
 
-	private ServerSocket server;
+    private ServerSocket server;
 
-	/**
-	 * Construtor privado para garantir o uso como singleton
-	 */
-	private ServidorChat() {
-	}
+    /**
+     * Construtor privado para garantir o uso como singleton
+     */
+    private ServidorChat() {
+    }
 
-	/**
-	 * ObtÃ©m a instancia singleton do classe ou a cria
-	 * 
-	 * @return ServidorChat
-	 */
-	public static ServidorChat getInstance() {
-		if (instance == null) {
-			instance = new ServidorChat();
-		}
+    /**
+     * ObtÃ©m a instancia singleton do classe ou a cria
+     * 
+     * @return ServidorChat
+     */
+    public static ServidorChat getInstance() {
+        if (instance == null) {
+            instance = new ServidorChat();
+        }
 
-		return instance;
-	}
+        return instance;
+    }
 
-	/**
-	 * Inicia a thread para escutar a porta
-	 * 
-	 * @param int porta
-	 * @param String nomeServidor
-	 *
-	 */
-	public void inicia(int porta, String nomeServidor) throws IOException {
-		this.porta = porta;
-		this.nomeServidor = nomeServidor;
+    /**
+     * Inicia a thread para escutar a porta
+     * 
+     * @param int porta
+     * @param String nomeServidor
+     *
+     */
+    public void inicia(int porta, String nomeServidor) throws IOException {
+        this.porta = porta;
+        this.nomeServidor = nomeServidor;
 
-		server = new ServerSocket(this.porta);
-		this.start();
-	}
+        server = new ServerSocket(this.porta);
+        this.start();
+    }
 
-	/**
-	 * Finaliza a thread a fecha o servidor
-	 * 
-	 * @throws IOException
-	 */
-	public void finaliza() throws IOException {
-		this.server.close();
-		this.interrupt();
-		instance = null;
-	}
+    /**
+     * Finaliza a thread a fecha o servidor
+     * 
+     * @throws IOException
+     */
+    public void finaliza() throws IOException {
+        this.server.close();
+        this.interrupt();
+        instance = null;
+    }
 
-	@Override
-	public void run() {
-		try {
-			while (true) {
-				// Espera uma conexão de socket
-				Socket socketCliente = this.server.accept();
-				
-				//Após receber a conexão cria uma janela de chat para o servidor
-				new TelaChat(socketCliente, TelaChat.TpTela.SERVIDOR,nomeServidor);
-				System.out.println("Socket no servidor: " + socketCliente);
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                // Espera uma conexão de socket
+                Socket socketCliente = this.server.accept();
+                
+                //Após receber a conexão cria uma janela de chat para o servidor
+                new TelaChat(socketCliente, TelaChat.TpTela.SERVIDOR,nomeServidor);
+                System.out.println("Socket no servidor: " + socketCliente);
 
-			}
-		} catch (Exception e) {
-			System.out.printf(e.getMessage());
-		}
-	}
+            }
+        } catch (Exception e) {
+            System.out.printf(e.getMessage());
+        }
+    }
 
-	public Socket getClient() {
-		return this.cliente;
-	}
+    public Socket getClient() {
+        return this.cliente;
+    }
 }
